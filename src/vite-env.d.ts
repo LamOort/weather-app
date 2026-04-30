de0c8df9169f99d21f93b4d457c9941d2cea298c
Lam Nguyen <nguyenchanhlam0811@gmail.com>
2026-04-30 16:59:42 +0300

    chore: trying to fix pipeline

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest/globals" />
 /// <reference types="@testing-library/jest-dom" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
